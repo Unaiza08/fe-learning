21764f3c9f0f1abf79149bc301fa381220c67a8f
Unaiza08 <i.unaiza.aginfotech@gmail.com>
2024-06-12 14:36:13 +0500

    Add files via upload

--- Calculator.js
+++ Calculator.js
@@ -9,16 +9,53 @@ function Solve(val)
     v.value += val;
 }
 function compute() {
-    var num1 = document.getElementById('output').value;
-     var num2 = eval(num1);
-     document.getElementById('output').value = num2;
-     
-    /*  [num1, operatorString, num2] = [num1, '+', num2] 
-        switch(operatorString) { 
-            case '+': return num1 + num2; 
-            case '-': return num1 - num2; 
-            case '*': return num1 * num2; 
-            case '/': return num1 / num2; 
-}  */
-    
+  
+    var expression = document.getElementById('output').value;
+
+    // Extract operator and operands
+    var [num1, operator, num2] = extractOperatorAndOperands(expression);
+
+    // Convert operands to numbers
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    // Perform the operation
+    var result;
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            result = 'Invalid operator';
+    }
+
+    // Update the output field with the result
+    document.getElementById('output').value = result;
+}
+
+// Function to extract operator and operands from the expression
+function extractOperatorAndOperands(expression) {
+    var operators = ['+', '-', '*', '/'];
+    var operator = '';
+    var num1 = '';
+    var num2 = '';
+    for (var i = 0; i < expression.length; i++) {
+        if (operators.includes(expression[i])) {
+            operator = expression[i];
+            num1 = expression.substring(0, i);
+            num2 = expression.substring(i + 1);
+            break;
+        }
+    }
+    return [num1, operator, num2];
+
 }
